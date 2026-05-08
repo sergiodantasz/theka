ce805f2701375b2c897c7263536e942b21fa6b8a
Sérgio Dantas <sergiodnts828@gmail.com>
2026-05-08 13:12:42 -0300

    style: set project typography

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,16 @@
 import type { Metadata } from "next";
+import { DM_Sans, Inter } from "next/font/google";
 import "./globals.css";
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Theka",
@@ -10,7 +21,7 @@ type Props = Readonly<{ children: React.ReactNode }>;
 
 export default function RootLayout({ children }: Props) {
   return (
-    <html lang='pt-br'>
+    <html lang='pt-br' className={`${dmSans.variable} ${inter.variable}`}>
       <body>{children}</body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi,
+        nesciunt.
+      </p>
+    </div>
+  );
 }
